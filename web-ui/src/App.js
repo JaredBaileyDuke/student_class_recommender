354a6e0755b641dfb8e6155ada63089582c81049
rootsec1 <nateriver210@gmail.com>
2024-04-16 10:48:59 -0400

    code cleanup

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -144,7 +144,7 @@ function CustomFormComponent({ setIsLoading, setResponseData }) {
           className="bg-gradient-to-br relative group/btn from-black to-neutral-600 block w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          Recommend courses &rarr;
           <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
           <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
         </button>
